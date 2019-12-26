ff19106c66b64feb3a1c4aa885c34bf737e3477a
Deep-Blue-2013 <deepbluechicago@gmail.com>
2019-12-26 13:59:55 -0600

    VideoCaptureFromFile

---
+++ src/SharpCV/NativeAPIs/cv2_native_api.videoio.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace SharpCV
+{
+    public partial class cv2_native_api
+    {
+        [DllImport(OpenCvDllName)]
+        internal static extern void videoio_VideoCapture_new2(string filename, int api, out IntPtr output);
+        [DllImport(OpenCvDllName)]
+        internal static extern void videoio_VideoCapture_read(IntPtr vid, IntPtr frame, out int output);
+    }
+}
--- test/UnitTest/Test.cs
+++ test/UnitTest/Test.cs
@@ -8,5 +8,6 @@ namespace UnitTest
     public abstract class Test
     {
         protected string imgSolar = Path.GetFullPath("../../../../../data/solar.jpg");
+        protected string mp4Road = Path.GetFullPath("../../../../../data/road.mp4");
     }
 }
