dc11d170e4ebb50529ab6bb508e430ad48ad47b7
Cocker Lee <OuTine@Gmail.com>
2020-01-22 00:34:50 +0800

    其实不太懂，但愿这两个参数赋值没写错

--- src/SharpCV/Structs/Size.cs
+++ src/SharpCV/Structs/Size.cs
@@ -14,5 +14,15 @@ namespace SharpCV
             Width = width;
             Height = height;
         }
+        /// <summary>
+        /// 自动实现元组解构
+        /// </summary>
+        /// <param name="ih"></param>
+        /// <param name="iw"></param>
+        public void Deconstruct(out int ih, out int iw)
+        {
+            ih = Height;
+            iw = Width;
+        }
     }
 }
