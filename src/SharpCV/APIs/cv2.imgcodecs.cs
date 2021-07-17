9cfa0f594f152f112158053c6623a57d6997ef86
久永 <jiuyong@hotmail.com>
2021-07-17 12:07:08 +0800

    add VectorOfByte for add ImDecode() . C# Version update 8.0 to 9.0 .

--- src/SharpCV/APIs/cv2.imgcodecs.cs
+++ src/SharpCV/APIs/cv2.imgcodecs.cs
@@ -24,5 +24,54 @@ namespace SharpCV
             cv2_native_api.core_Mat_new7(src, rect, out var handle);
             return new Mat(handle);
         }
+
+        /// <summary>
+        /// Reads image from the specified buffer in memory.
+        /// </summary>
+        /// <param name="span">The input slice of bytes.</param>
+        /// <param name="flags">The same flags as in imread</param>
+        /// <returns></returns>
+        public static Mat ImDecode(ReadOnlySpan<byte> span, IMREAD_COLOR flags)
+        {
+            if (span.IsEmpty)
+                throw new ArgumentException("Empty span", nameof(span));
+
+            unsafe
+            {
+                fixed (byte* pBuf = span)
+                {
+                        cv2_native_api.imgcodecs_imdecode_vector(pBuf, span.Length, flags, out var ret);
+                    return new Mat(ret);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Compresses the image and stores it in the memory buffer
+        /// </summary>
+        /// <param name="ext">The file extension that defines the output format</param>
+        /// <param name="img">The image to be written</param>
+        /// <param name="buf">Output buffer resized to fit the compressed image.</param>
+        /// <param name="prms">Format-specific parameters.</param>
+        public static bool ImEncode(string ext, Mat img, out byte[] buf, int[]? prms = null)
+        {
+            if (string.IsNullOrEmpty(ext))
+                throw new ArgumentNullException(nameof(ext));
+            if (img == null)
+                throw new ArgumentNullException(nameof(img));
+            if (prms == null)
+                prms = Array.Empty<int>();
+			//img.ThrowIfDisposed();
+
+			using var bufVec = new VectorOfByte();
+			var exe_result =
+                cv2_native_api.imgcodecs_imencode_vector(ext, img.OutputArray, bufVec.Handle, prms, prms.Length, out var ret);
+            GC.KeepAlive(img);
+
+			buf = bufVec.ToArray();
+
+			return ret != 0;
+        }
+
     }
 }
---
+++ src/SharpCV/Vectors/IStdVector.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SharpCV
+{
+    /// <summary>
+    /// Represents std::vector 
+    /// </summary>
+    public interface IStdVector<out T> : IDisposable
+    {
+        /// <summary>
+        /// vector.size()
+        /// </summary>
+        int Size { get; }
+
+        /// <summary>
+        /// Convert std::vector&lt;T&gt; to managed array T[]
+        /// </summary>
+        /// <returns></returns>
+        T[] ToArray();
+    }
+}
